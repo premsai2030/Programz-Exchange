9522a3103da0635551da667052bda2f6df2d57f2
premsai2030 <66376642+premsai2030@users.noreply.github.com>
2021-01-21 19:09:51 +0530

    enter

--- HOME/AURDINO/index.php
+++ HOME/AURDINO/index.php
@@ -26,7 +26,19 @@ $result=mysqli_query($db,$sql);
  </head>
  <body>
   <style type="text/css">
-  
+       p img
+     {
+         max-width:100%;
+         heiht:100%;
+     }
+  #fa1
+{
+	padding :10px;
+	width: 30px;
+	font-size:25px;
+	margin-left: 10px;
+	text-decoration: none;
+}
 @media screen and (min-width:990px){
 #open{
 display:none;
@@ -58,12 +70,12 @@ font-size: 10px;
     </center>
   </div>
   <div class="col-sm-6 ">
-    <a href="http://localhost/programz-website/HOME" id="close" class="navbar-brand" style="margin-top: 20px;font-size: 17px;margin-left:30px;color: white;">Home</a>
-<a href="http://localhost/programz-website/HOME/PUBLIC/" id="close" class="navbar-brand" style="margin-top: 20px;font-size: 17px;color: white;">
+    <a href="http://programz-exchange.epizy.com/HOME/" id="close" class="navbar-brand" style="margin-top: 20px;font-size: 17px;margin-left:30px;color: white;">Home</a>
+<a href="http://programz-exchange.epizy.com/HOME/PUBLIC/" id="close" class="navbar-brand" style="margin-top: 20px;font-size: 17px;color: white;">
 Public</a>
-<a  href="http://localhost/programz-website/HOME/PRIVATE/" id="close" class="navbar-brand" style="margin-top: 20px;font-size: 17px;color: white;">
+<a  href="http://programz-exchange.epizy.com/HOME/PRIVATE/" id="close" class="navbar-brand" style="margin-top: 20px;font-size: 17px;color: white;">
 Private</a>
-                      <a href="http://localhost/programz-website/LOGOUT" id="close" class="navbar-brand" style="margin-top: 20px;font-size: 17px;color: white;">Logout
+                      <a href="http://programz-exchange.epizy.com/LOGOUT" id="close" class="navbar-brand" style="margin-top: 20px;font-size: 17px;color: white;">Logout
 </a>
     
   </div>
@@ -110,7 +122,7 @@ while ($row=mysqli_fetch_assoc($result)) {
       <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#<?php echo $row["id"]; ?>"
         aria-expanded="false" aria-controls="collapseThree3" style="text-decoration: none;">
         <p class="mb-0"  style="color: blue"><b>
-        <?php echo $row["title"]; ?>
+        <?php echo str_replace("\n","<br>",$row["title"]); ?>
 </b>
         </p>
 
@@ -118,20 +130,38 @@ while ($row=mysqli_fetch_assoc($result)) {
 <div style="margin-top: 10px;">
         
 <center>
-        <span style="float: left;"><span><b>From :</b> <?php echo $row["username"]; ?></span></span><span style="float: right;">posted on <?php echo $row["time1"]; ?></span>
+        <span ><span style="float: left;"><b>From :</b> <?php echo $row["username"]; ?></span><a style="float:right;font-size: 25px;" class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#links<?php echo $row["id"]; ?>"
+	aria-expanded="false" aria-controls="collapseThree3"   ><i class="fa fa-share-alt" aria-hidden="true"></i>
+	</a></span>
 </center>
 </div>
     </div>
+<div id="links<?php echo $row["id"]; ?>" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+      data-parent="#accordionEx">
+      <div class="card-body">
 
+<a href=<?php echo "whatsapp://send?text=http://programz-exchange.epizy.com/share.php?id=".$row["id"].""; ?>  class="fa fa-whatsapp" style="color:green" id="fa1"></a>
+<a href=<?php echo "tg://msg_url?url=http://programz-exchange.epizy.com/share.php?id=".$row["id"]."" ; ?> class="fa fa-telegram" id="fa1" ></a>
+
+
+	</div>
+  </div>
     <!-- Card body -->
     <div id="<?php echo $row["id"]; ?>" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
       data-parent="#accordionEx">
       <div class="card-body">
-<p><?php echo $row["description"]; ?></p>
-              <textarea  rows="15" style=" resize: none; width: 100%;" disabled="" >
+<p><?php echo str_replace("\n","<br>",$row["description"]);
+ ?></p>
+              <textarea  rows="
+              <?php
+              $rows= substr_count($row["source_code"],"\n") +2;
+              echo $rows ;
+              ?>
+              " style=" resize: none; width: 100%;" disabled="" >
 <?php echo $row["source_code"]; ?>
     </textarea>
-    <center>
+  <center>
+    <span> <b>posted on : </b><?php echo $row["time1"]; ?></span>
     </center>
       </div>
    
@@ -156,26 +186,26 @@ while ($row=mysqli_fetch_assoc($result)) {
   <div class="row" style="padding: 10px;">
     <div class="col-3">
 <center>
-  <a href="http://localhost/programz-website/HOME"  class="navbar-brand" style="color: white;font-size: 15px;">
+  <a href="http://programz-exchange.epizy.com/HOME/"  class="navbar-brand" style="color: white;font-size: 15px;">
 Home</a>
 </center>   </div>
     <div class="col-3">
 <center>
-<a href="http://localhost/programz-website/HOME/PUBLIC/"  class="navbar-brand" style="color: white;font-size: 15px;">
+<a href="http://programz-exchange.epizy.com/HOME/PUBLIC/"  class="navbar-brand" style="color: white;font-size: 15px;">
 Public</a>
 </center>
 
     </div>
         <div class="col-3">
 <center>
-<a  href="http://localhost/programz-website/HOME/PRIVATE/"  class="navbar-brand" style="color: white;font-size: 15px;">
+<a  href="http://programz-exchange.epizy.com/HOME/PRIVATE/"  class="navbar-brand" style="color: white;font-size: 15px;">
 Private</a>
 </center>
 
     </div>
         <div class="col-3">
 <center>
-<a href="http://localhost/programz-website/LOGOUT" class="navbar-brand" style="color: white;font-size: 15px;">Logout
+<a href="http://programz-exchange.epizy.com/LOGOUT" class="navbar-brand" style="color: white;font-size: 15px;">Logout
 </a>
 </center>
 
